1a64508107b77981a91d569d5127f70dbe305592
Torben Sell <torbensell@dhcp-172-24-143-143.eduroam.wireless.private.cam.ac.uk>
2017-10-31 16:52:08 +0000

    Bens and Torbens work

--- example.py
+++ example.py
@@ -12,7 +12,7 @@ from collections import defaultdict
 from PyQt5.QtWidgets import QApplication
 
 from maze import Maze, Game, game_repeater
-from goodies import RandomGoody
+from goodies import RandomGoody, TorBenGoody, TorBenGoody2
 from baddies import RandomBaddy
 from gui import GameViewer
 
@@ -31,8 +31,8 @@ EXAMPLE_MAZE = Maze(10, 10, "0001010000"
 def text_example():
     ''' Prints the state of the game to stdout after each round of turns '''
 
-    goody0 = RandomGoody()
-    goody1 = RandomGoody()
+    goody0 = TorBenGoody2()
+    goody1 = TorBenGoody2()
     baddy = RandomBaddy()
 
     game = Game(EXAMPLE_MAZE * (2, 2), goody0, goody1, baddy)
@@ -47,13 +47,13 @@ def stats_example(total_games):
     ''' Plays many games, printing cumulative and final stats '''
 
     results = defaultdict(int)
-    for game_number, game in enumerate(game_repeater(EXAMPLE_MAZE, RandomGoody, RandomGoody, RandomBaddy)):
+    for game_number, game in enumerate(game_repeater(EXAMPLE_MAZE, TorBenGoody2, TorBenGoody2, RandomBaddy)):
         if game_number == total_games:
             break
         result, _rounds = game.play()
         results[result] += 1
-        if game_number % 10 == 0:
-            print(game_number, "/", total_games, ":", dict(results))
+      #  if game_number % 10 == 0:
+      #      print(game_number, "/", total_games, ":", dict(results))
 
     print(dict(results))
 
@@ -62,11 +62,11 @@ def gui_example():
     app = QApplication.instance() or QApplication(sys.argv)
     gv = GameViewer()
     gv.show()
-    gv.set_game_generator(game_repeater(EXAMPLE_MAZE * (3, 3), RandomGoody, RandomGoody, RandomBaddy))
+    gv.set_game_generator(game_repeater(EXAMPLE_MAZE * (3, 3), TorBenGoody2, TorBenGoody2, RandomBaddy))
     app.exec_()
 
 if __name__ == "__main__":
     # Uncomment whichever example you want to run
-    text_example()
-    #stats_example(1000)
+    #text_example()
+    stats_example(10000)
     #gui_example()
--- goodies.py
+++ goodies.py
@@ -20,5 +20,171 @@ class RandomGoody(Goody):
 
     def take_turn(self, obstruction, _ping_response):
         ''' Ignore any ping information, just choose a random direction to walk in, or ping '''
-        possibilities = [direction for direction in [UP, UP, RIGHT, DOWN, LEFT, RIGHT] if not obstruction[direction]] + [PING]
+
+        possibilities = [direction for direction in [UP, DOWN, LEFT, RIGHT] if not obstruction[direction]] + [PING]
         return random.choice(possibilities)
+
+class TorBenGoody(Goody):
+    ''' A random-walking goody '''
+    
+    def __init__(self):
+        self.count = 0
+        self.corner = None
+        self.middlepoint_x = None
+        self.middlepoint_y = None
+        self.position_x = 0
+        self.position_y = 0
+        self.withinmiddlepoint = False
+
+    def take_turn(self, obstruction, _ping_response):
+        ''' Ignore any ping information, just choose a random direction to walk in, or ping '''
+        
+        if self.count==0 and _ping_response == None:
+            self.count += 1
+            return PING
+        elif _ping_response != None:
+            self.count += 1
+            for player, position in _ping_response.items():
+                if isinstance(player, Goody):
+                    goody_position = position
+                else:
+                    baddy_position = position
+            
+            self.middlepoint_x = goody_position.x/2
+            self.middlepoint_y = goody_position.y/2
+            
+            if goody_position.y/2<baddy_position.y:
+                if goody_position.x/2<baddy_position.x:
+                    self.corner = 'bottomleft'
+                else:
+                    self.corner = 'bottomright'
+            else:
+                if goody_position.x/2<baddy_position.x:
+                    self.corner = 'topleft'
+                else:
+                    self.corner = 'topright'
+        
+        '''Check whether goody has reached meeting area, if not, proceed there, o/w go to side'''
+        if abs(self.middlepoint_x-self.position_x)**2+abs(self.middlepoint_y-self.position_y)**2<2:
+            self.withinmiddlepoint = True
+        
+        
+        if self.withinmiddlepoint == False:
+            u = random.uniform(0,1)
+            if u<0.7:
+                u = u/0.7
+                if u < abs(self.middlepoint_x-self.position_x)/(abs(self.middlepoint_x-self.position_x)+abs(self.middlepoint_y-self.position_y)):
+                    if self.middlepoint_x-self.position_x > 0 and not obstruction[RIGHT]:
+                        self.position_x += 1
+                        return RIGHT
+                    elif not obstruction[LEFT]:
+                        self.position_x -= 1
+                        return LEFT
+                else:
+                    if self.middlepoint_y-self.position_y > 0 and not obstruction[UP]:
+                        self.position_y += 1
+                        return UP
+                    elif not obstruction[DOWN]:
+                        self.position_y -= 1
+                        return DOWN   
+                        
+        
+        if self.withinmiddlepoint == True:           
+            if self.corner == 'bottomleft':
+                possibilities = [direction for direction in [LEFT, DOWN, UP, DOWN, LEFT, RIGHT] if not obstruction[direction]] 
+                return random.choice(possibilities)
+            elif self.corner == 'topleft':
+                possibilities = [direction for direction in [LEFT, UP, UP, DOWN, LEFT, RIGHT] if not obstruction[direction]] 
+                return random.choice(possibilities)
+            elif self.corner == 'bottomright':
+                possibilities = [direction for direction in [RIGHT, DOWN, UP, DOWN, LEFT, RIGHT] if not obstruction[direction]] 
+                return random.choice(possibilities)
+            else:
+                possibilities = [direction for direction in [RIGHT, UP, UP, DOWN, LEFT, RIGHT] if not obstruction[direction]] 
+                return random.choice(possibilities)
+        
+        
+        '''Every now and then we get here and just do a random move'''
+        possibilities = [direction for direction in [UP, DOWN, LEFT, RIGHT] if not obstruction[direction]] 
+        return random.choice(possibilities)
+
+
+class TorBenGoody2(Goody):
+    ''' A random-walking goody '''
+    
+    def __init__(self):
+        self.count = 0
+        self.corner = None
+        self.middlepoint_x = None
+        self.middlepoint_y = None
+        self.position_x = 0
+        self.position_y = 0
+        self.withinmiddlepoint = False
+
+    def take_turn(self, obstruction, _ping_response):
+        ''' Ignore any ping information, just choose a random direction to walk in, or ping '''
+        
+        if self.count==0 and _ping_response == None:
+            self.count += 1
+            return PING
+        elif _ping_response != None:
+            self.count += 1
+            for player, position in _ping_response.items():
+                if isinstance(player, Goody):
+                    goody_position = position
+                else:
+                    baddy_position = position
+            
+            self.middlepoint_x = goody_position.x/2
+            self.middlepoint_y = goody_position.y/2
+            
+            if abs(goody_position.x/2-baddy_position.x)>abs(goody_position.y/2-baddy_position.y):
+                if goody_position.x/2<baddy_position.x:
+                    self.corner = 'left'
+                else:
+                    self.corner = 'right'
+            else:
+                if goody_position.y/2<baddy_position.y:
+                    self.corner = 'bottom'
+                else:
+                    self.corner = 'top'
+        
+        '''Check whether Goody has reached meeting area, if not, go there, o/w go to side'''
+        if abs(self.middlepoint_x-self.position_x)**2+abs(self.middlepoint_y-self.position_y)**2<2:
+            self.withinmiddlepoint = True
+        else:
+            self.withinmiddlepoint = False
+        
+        
+        if self.withinmiddlepoint == False:
+            u = random.uniform(0,1)
+            if u<0.7:
+                u = u/0.7
+                if u < abs(self.middlepoint_x-self.position_x)/(abs(self.middlepoint_x-self.position_x)+abs(self.middlepoint_y-self.position_y)):
+                    if self.middlepoint_x-self.position_x > 0 and not obstruction[RIGHT]:
+                        self.position_x += 1
+                        return RIGHT
+                    elif not obstruction[LEFT]:
+                        self.position_x -= 1
+                        return LEFT
+                else:
+                    if self.middlepoint_y-self.position_y > 0 and not obstruction[UP]:
+                        self.position_y += 1
+                        return UP
+                    elif not obstruction[DOWN]:
+                        self.position_y -= 1
+                        return DOWN   
+                        
+        
+        '''Whenever we are in the meetingarea just do a random move'''
+        possibilities = [direction for direction in [UP, DOWN, LEFT, RIGHT] if not obstruction[direction]]
+        dir = random.choice(possibilities)
+        if dir == UP:
+            self.position_y += 1
+        elif dir == DOWN:
+            self.position_y -= 1
+        elif dir == LEFT:
+            self.position_x -= 1
+        elif dir == RIGHT:
+            self.position_x += 1
+        return dir
